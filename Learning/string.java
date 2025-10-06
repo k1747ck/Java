b3f80833fd570ebdaad620d7d9ce7f5eec24a073
k1747ck <khushikadam4236@gmail.com>
2025-10-06 09:33:05 +0530

    string

---
+++ Learning/string.java
@@ -0,0 +1,10 @@
+public class string {
+    public static void main(String a[]) {
+      
+        String name = new String("khushi");
+        System.out.println("hello " +name);
+
+        System.out.println(name.charAt(0));
+        System.out.println(name.concat("kadam"));
+    }
+}
