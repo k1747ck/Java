4b1f13a10cd395a8c282061185b59e0ebe9254e1
k1747ck <khushikadam4236@gmail.com>
2025-11-13 22:27:35 +0530

    updates

---
+++ Ubuntu-progs/Bank_Account.java
@@ -0,0 +1,53 @@
+// import java.util.Scanner;
+// class Account{
+    
+//     // String Name ;
+//     // System.out.println("Enter your Name: "+ Name);
+//     // int Number ;
+//     // System.out.println("Enter your Phone Number: "+ Number);
+//     // int Aadhar ;
+//     // System.out.println("Enter your Aadhar Number: "+ Aadhar);
+//     // String Fname ;
+//     // System.out.println("Enter you Fathers Name: "+ Fname);
+//     // int IFSC ;
+//     // System.out.println("Enter your IFSC Code: "+ IFSC);
+//     // String Branch ;
+//     // System.out.println("Enter your Branch Name: "+ Branch);
+//     // String Email ;
+//     // System.out.println("Enter your Email Id: "+ Email);
+//     int Balance
+//     public int Balance(int)
+// }
+// public class Bank_Account {
+//     public static void main(String[] args){
+        
+//     }
+// }
+
+
+
+
+import java.util.Scanner;
+public class Bank_Account{
+
+    class Bank{
+        String First_Name;
+        String Last_Name;
+        int Phone_Number;
+        int Aadhar;
+        int Age;
+        String Address;
+        String Fname;
+        String Branch;
+        int IFSC_Code;
+        int Account_Number;
+        int Current_Balance;
+
+
+
+        public Bank(String fn , String ln){
+            First_Name = fn;
+            Last_Name = ln;
+        }
+    }
+}
